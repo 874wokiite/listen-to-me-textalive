f540412e5e602e7bcd548184a94eb4bc86c92b73
874wokiite <f.kanonon@gmail.com>
2024-05-06 11:36:07 +0900

    feat: スクロールチャレンジ（あとで消すと思う）

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Body from "@/comonents/Body";
+import ScrollChange from "@/comonents/ScrollChange";
 // import LogApp from "@/components/LogApp";
 
 const App = () => {
   return (
     <div>
       <h1>TextAlive App</h1>
-      <Body />
+      {/* <Body /> */}
+      <ScrollChange />
     </div>
   );
 };
